2c4bfab8d1e4308b3d6016b82e6de855d9530c93
Hasee28 <tohazsha@gmail.com>
2025-11-11 17:58:06 +0530

    Code for duplicate

---
+++ leetcode problems/Day-1/containsduplicate.java
@@ -0,0 +1,47 @@
+//Brute Force Approach- O(n^2) time complexity, O(1) space complexity
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] == nums[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
+
+
+//Optimal Approach- O(n log n) time complexity, O(1) space complexity
+import java.util.Arrays;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+//Best Approach- O(n) time complexity, O(n) space complexity
+import java.util.HashSet;
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return true;
+            }
+            set.add(num);
+        }
+        return false;
+    }
+}
+
+
