e803267865bc7bb768e5f7c7b67b8828a2c2c0a2
Hasee28 <tohazsha@gmail.com>
2025-11-14 00:24:14 +0530

    Matrix zeroes-Using extra space

--- leetcode problems/Day-3/Matrix-zeroes.java
+++ leetcode problems/Day-3/Matrix-zeroes.java
@@ -36,3 +36,32 @@ class Solution {
     }
 }
 
+//Using Extra Space-time complexity-O(m*n) ,space complexity-O(m+n)
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean[] rows = new boolean[m];
+        boolean[] cols = new boolean[n];
+
+        // First pass: mark rows & columns
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    rows[i] = true;
+                    cols[j] = true;
+                }
+            }
+        }
+
+        // Second pass: set zeros based on marks
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (rows[i] || cols[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
+
