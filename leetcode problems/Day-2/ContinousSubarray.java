89526f82d5b9a5086932a52daa6bc1082a9864d4
Hasee28 <tohazsha@gmail.com>
2025-11-13 18:50:10 +0530

    Continous Subarray

--- leetcode problems/Day-2/ContinousSubarray.java
+++ leetcode problems/Day-2/ContinousSubarray.java
@@ -14,3 +14,24 @@ class Solution {
     }
 }
 
+//time complexity: O(n^2)
+//space complexity: O(n)
+    public boolean checkSubarraySum(int[] nums, int k) {
+        int n = nums.length;
+        int[] prefix = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            prefix[i] = prefix[i - 1] + nums[i - 1];
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 2; j <= n; j++) { 
+                if ((prefix[j] - prefix[i]) % k == 0)
+                    return true;
+            }
+        }
+
+        return false;
+    }
+
+
