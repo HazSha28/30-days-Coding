cf31f04b34d1a959ce6a20e6a1953bf89e7babef
Hasee28 <tohazsha@gmail.com>
2025-11-18 21:06:27 +0530

    Rotateimage problem

--- leetcode problems/Day-3/Rotate-Image.java
+++ leetcode problems/Day-3/Rotate-Image.java
@@ -21,6 +21,43 @@ class Solution {
     }
 }
 
+//
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        for (int i = 0; i < (n+1)/2; i++) {
+            for (int j = 0; j < n/2; j++) {
+
+                int temp = matrix[n - 1 - j][i];
+
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j];
+                matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
+                matrix[j][n - 1 - i] = matrix[i][j];
+                matrix[i][j] = temp;
+            }
+        }
+    }
+}
+
+//Another Approach: Rotate 4 elements at a time
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        for (int i = 0; i < (n+1)/2; i++) {
+            for (int j = 0; j < n/2; j++) {
+
+                int temp = matrix[n - 1 - j][i];
+
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j];
+                matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
+                matrix[j][n - 1 - i] = matrix[i][j];
+                matrix[i][j] = temp;
+            }
+        }
+    }
+}
 
 //Optimized Approach: Transpose the matrix and then reverse each row
 class Solution {
